1154c833a86ec8a820f9245c182cedd630e96bc5
Piotr Kosmowski <kospiotr@tlen.pl>
2019-11-26 23:58:21 +0100

    Init

---
+++ src/pages/Console.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <api-console no-toolbars :model-location="model_location"></api-console>
+  </q-page>
+</template>
+<script>
+/*eslint-disable */
+export default {
+  name: 'Console',
+  data() {
+    return {
+      model_location: "/statics/amf-model.json"
+    }
+  }
+
+}
+</script>
